99ebf65bc7a77bc8da0d674ad542c57ac41f68ac
Akshayingle666 <127146295+Akshayingle666@users.noreply.github.com>
2023-03-29 00:49:30 +0530

    initial update

---
+++ FristServlet/src/main/java/Servlet1/FirstServlet.java
@@ -0,0 +1,25 @@
+package Servlet1;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+@WebServlet("/first")
+public class FirstServlet implements Servlet  {
+	public void init(ServletConfig sc) throws ServletException {}
+    public void service(ServletRequest req,ServletResponse res) throws ServletException,IOException {
+    	String uName=req.getParameter("uname");
+    	String mId=req.getParameter("mid");
+    	PrintWriter pw=res.getWriter();
+        res.setContentType("text/html");
+        pw.println("*****UserDetails");
+        pw.println("<br>UserName:"+uName);
+        pw.println("<br>MailId:"+mId);
+    }
+    public void destroy() {}
+    public String getServletInfo() {
+    	return "FirstServlet";
+    }
+    public ServletConfig getServletConfig() {
+    	return this.getServletConfig();
+    }
+
+}
